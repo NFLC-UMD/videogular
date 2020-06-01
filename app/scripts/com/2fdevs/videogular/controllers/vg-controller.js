ff9563cc978995b80a0c57cce6d544a5cbd210c0
thomaswford <tford@nflc.umd.edu>
2020-06-01 11:08:03 -0400

    Prevent error from loading index.html into video element

--- app/scripts/com/2fdevs/videogular/controllers/vg-controller.js
+++ app/scripts/com/2fdevs/videogular/controllers/vg-controller.js
@@ -82,21 +82,25 @@ angular.module("com.2fdevs.videogular")
         this.videogularElement = null;
 
         this.clearMedia = function () {
-            this.mediaElement[0].src = '';
-            this.mediaElement[0].removeEventListener("canplay", this.onCanPlay.bind(this), false);
-            this.mediaElement[0].removeEventListener("loadedmetadata", this.onLoadMetaData.bind(this), false);
-            this.mediaElement[0].removeEventListener("waiting", this.onStartBuffering.bind(this), false);
-            this.mediaElement[0].removeEventListener("ended", this.onComplete.bind(this), false);
-            this.mediaElement[0].removeEventListener("playing", this.onStartPlaying.bind(this), false);
-            this.mediaElement[0].removeEventListener("play", this.onPlay.bind(this), false);
-            this.mediaElement[0].removeEventListener("pause", this.onPause.bind(this), false);
-            this.mediaElement[0].removeEventListener("volumechange", this.onVolumeChange.bind(this), false);
-            this.mediaElement[0].removeEventListener("playbackchange", this.onPlaybackChange.bind(this), false);
-            this.mediaElement[0].removeEventListener("timeupdate", this.onUpdateTime.bind(this), false);
-            this.mediaElement[0].removeEventListener("progress", this.onProgress.bind(this), false);
-            this.mediaElement[0].removeEventListener("seeking", this.onSeeking.bind(this), false);
-            this.mediaElement[0].removeEventListener("seeked", this.onSeeked.bind(this), false);
-            this.mediaElement[0].removeEventListener("error", this.onVideoError.bind(this), false);
+            try {
+                this.mediaElement[0].pause();
+                this.mediaElement[0].removeAttribute('src');
+                this.mediaElement[0].removeEventListener("canplay", this.onCanPlay.bind(this), false);
+                this.mediaElement[0].removeEventListener("loadedmetadata", this.onLoadMetaData.bind(this), false);
+                this.mediaElement[0].removeEventListener("waiting", this.onStartBuffering.bind(this), false);
+                this.mediaElement[0].removeEventListener("ended", this.onComplete.bind(this), false);
+                this.mediaElement[0].removeEventListener("playing", this.onStartPlaying.bind(this), false);
+                this.mediaElement[0].removeEventListener("play", this.onPlay.bind(this), false);
+                this.mediaElement[0].removeEventListener("pause", this.onPause.bind(this), false);
+                this.mediaElement[0].removeEventListener("volumechange", this.onVolumeChange.bind(this), false);
+                this.mediaElement[0].removeEventListener("playbackchange", this.onPlaybackChange.bind(this), false);
+                this.mediaElement[0].removeEventListener("timeupdate", this.onUpdateTime.bind(this), false);
+                this.mediaElement[0].removeEventListener("progress", this.onProgress.bind(this), false);
+                this.mediaElement[0].removeEventListener("seeking", this.onSeeking.bind(this), false);
+                this.mediaElement[0].removeEventListener("seeked", this.onSeeked.bind(this), false);
+                this.mediaElement[0].removeEventListener("error", this.onVideoError.bind(this), false);
+                this.mediaElement[0].load(); // empty src to clear current source    
+            } catch(e) {}
         };
 
         this.onRouteChange = function() {
